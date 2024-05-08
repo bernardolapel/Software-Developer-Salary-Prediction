946c3e3111120f5ca484ce600dcf029db7f8fcae
bernardolapel <114789731+bernardolapel@users.noreply.github.com>
2024-05-07 22:22:57 -0600

    Update predict_page.py

--- predict_page.py
+++ predict_page.py
@@ -3,8 +3,7 @@ import pickle
 import numpy as np
 
 def load_model():
-    with open('model.pk1', 'rb') as file:
-        data = pickle.load(file)
+    data = pickle.load('model.pk',rb)
     return data
     
 data = load_model()
